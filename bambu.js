ff8e9a3bcf2271b5d2db656e0e13253e97256994
brendan <brendan.heberton@gmail.com>
2014-04-29 09:38:38 -0600

    update license in bambu js

--- bambu.js
+++ bambu.js
@@ -1,10 +1,13 @@
 /** 
-  Bambu generates thematic carto.js strings
+  Bambu generates thematic carto.js strings and Esri simple renderers.
 
-  Currently under active development and is being used to style 
-  vector tile json data in modestmaps via Carto.js & VECNIK.js.
+  Currently under active development.
 
-  MIT License - Copryright 2012 
+  Copyright 2014 Esri
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0 
 */
 function Bambu() {
 
